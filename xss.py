1a5b74a15d65286d81ddd00d5639c217b15873dd
Akash <ssj2akash@gmail.com>
2022-09-20 14:57:44 +0530

    XSS: Recursion baby!!!
    recursively identify all the dom tags available, always look like black magic after coding lol

--- xss.py
+++ xss.py
@@ -1,10 +1,24 @@
+scancount = 0
+
 def scanfunction(content, tag):
+
     l1 = content.find(tag + '(')
     ln = content[l1:].find(')')
+    last = l1+ln+1
+
+    #print(f'start index:{l1}, end index:{last}')
     if(l1 == -1):
-        return 'not found xss vuln'
-    return content[l1:ln+1]
+        return None, None
+    return content[l1:l1+ln+1], last
 
 
 def scanforxss(area):
-    return scanfunction(area, 'getElementById')
+    domtag1 = 'getElementById'
+    line = 0
+    tag = ''
+
+    while(tag != None):
+        print(tag, line)
+        area = area[line:]
+        tag, line = scanfunction(area, domtag1)
+        #print("line after call is:",line)
